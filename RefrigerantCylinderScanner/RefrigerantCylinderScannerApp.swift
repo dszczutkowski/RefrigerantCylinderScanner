993e296ef8958279985ca078a42db9d8359738ae
Dave <daveszczutkowski@gmail.com>
2022-07-05 18:22:15 +0200

    Initial Commit

---
+++ RefrigerantCylinderScanner/RefrigerantCylinderScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefrigerantCylinderScannerApp.swift
+//  RefrigerantCylinderScanner
+//
+//  Created by Dave Szczutkowski on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RefrigerantCylinderScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
